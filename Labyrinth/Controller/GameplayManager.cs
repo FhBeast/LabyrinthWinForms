96798eda8c4da509336ab2e5de9c6d81d3120a5c
FhBeast <fhdavidqqq@gmail.com>
2022-12-07 23:49:26 +0300

    create GameplayManager

---
+++ Labyrinth/Controller/GameplayManager.cs
@@ -0,0 +1,17 @@
+﻿using Labyrinth.Model;
+
+namespace Labyrinth.Controller;
+
+internal static class GameplayManager
+{
+    public static bool LevelIsComplete(List<Coin> coins)
+    {
+        if (coins is null)
+        {
+            throw new ArgumentNullException($"{nameof(coins)}");
+        }
+
+        if (coins.Count < 1) return true;
+        return false;
+    }
+}
