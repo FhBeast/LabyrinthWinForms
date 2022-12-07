68a6db991d19a631520c0cf433d12de290f55a6a
FhBeast <fhdavidqqq@gmail.com>
2022-12-07 13:31:00 +0300

    transfer Form

--- Labyrinth/View/Form1.cs
+++ Labyrinth/View/Form1.cs
@@ -10,14 +10,22 @@ public partial class Form1 : Form
         InitializeComponent();
         Player = new(0, 0);
         Enemy = new(10, 10);
+        Field = new();
+        Coins = new();
+        Coins.Add(new Coin(5, 5));
+        Coins.Add(new Coin(50, 5));
+        Coins.Add(new Coin(5, 25));
     }
 
+    private bool ReadyToDraw { get; set; }
     private int WidthBitmap { get; set; }
     private int HeightBitmap { get; set; }
-    private Field Field = new();
+    private Field Field { get; set; }
     private Point MousePictureBoxPosition { get; set; }
     private Player Player { get; set; }
     private Enemy Enemy { get; set; }
+
+    private List<Coin> Coins { get; set; }
 
     private void Form1_Load(object sender, EventArgs e)
     {
@@ -25,8 +33,8 @@ public partial class Form1 : Form
         WidthBitmap = pictureBox1.Width;
 
         Field = new(30, 60);
-        Field.GenerateDepthSearch();
-        Field.AddBraidng();
+        FieldGenerator.GenerateDepthSearch(Field);
+        FieldGenerator.AddBraidng(Field);
         Enemy.SetSlowdown(1);
         StartWave();
     }
@@ -38,24 +46,31 @@ public partial class Form1 : Form
         Drawing.DrawLabytinth(bitmap, Field);
         Drawing.DrawPlayer(bitmap, Player);
         Drawing.DrawEnemy(bitmap, Enemy);
+        foreach (var coin in Coins)
+        {
+            Drawing.DrawCoin(bitmap, coin);
+        }
         pictureBox1.Image = bitmap;
     }
 
     private void Timer1_Tick(object sender, EventArgs e)
     {
-        Draw();
+        if(ReadyToDraw)
+            Draw();
     }
 
     private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
     {
         MousePictureBoxPosition = new(e.X, e.Y);
     }
-
+    
     private void StartWave()
     {
+        ReadyToDraw = false;
         Task.Run(() =>
         {
             Field.WaveTracingFast();
+            ReadyToDraw = true;
         });
     }
 
