4c100a5c4201cd439624c178b5e97f2638f54b69
FhBeast <fhdavidqqq@gmail.com>
2022-12-08 16:55:18 +0300

    add CollisionAction method to BasePlayableEntity.cs and Coin.cs

--- Labyrinth/Model/Coin.cs
+++ Labyrinth/Model/Coin.cs
@@ -5,4 +5,12 @@ internal class Coin : BasePlayableEntity
     public bool IsPicked { get; set; }
     
     public Coin(int x, int y) : base(x, y) { }
+
+    public override void CollisionAction(BasePlayableEntity entity)
+    {
+        if (entity is Player)
+        {
+            IsPicked = true;
+        }
+    }
 }
