15b13fda04f32a8210f67bb0c2c9f1318fd6daa2
FhBeast <fhdavidqqq@gmail.com>
2022-12-08 16:54:05 +0300

    modify GameplayManager

--- Labyrinth/Controller/GameplayManager.cs
+++ Labyrinth/Controller/GameplayManager.cs
@@ -1,4 +1,5 @@
 ﻿using Labyrinth.Model;
+using Labyrinth.Util;
 
 namespace Labyrinth.Controller;
 
@@ -12,6 +13,47 @@ internal static class GameplayManager
         }
 
         if (coins.Count < 1) return true;
+        return false;
+    }
+
+    public static bool CoinIsPicked(List<Coin> coins, Player player)
+    {
+        if (coins is null)
+        {
+            throw new ArgumentNullException($"{nameof(coins)}");
+        }
+
+        if (player is null)
+        {
+            throw new ArgumentNullException($"{nameof(player)}");
+        }
+
+        if (CollisionVerificator.Collision(player, coins.Cast<BasePlayableEntity>().ToList()))
+        {
+            coins.RemoveAll(x => x.IsPicked);
+            return true;
+        }
+
+        return false;
+    }
+
+    public static bool GameIsOver(Enemy enemy, Player player)
+    {
+        if (enemy is null)
+        {
+            throw new ArgumentNullException($"{nameof(enemy)}");
+        }
+
+        if (player is null)
+        {
+            throw new ArgumentNullException($"{nameof(player)}");
+        }
+
+        if (CollisionVerificator.Collision(player, enemy))
+        {
+            return true;
+        }
+
         return false;
     }
 }
